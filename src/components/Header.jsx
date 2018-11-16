821e8ddd148d714587e066631cc57b0761dd8051
mobydickens <steadmegan@gmail.com>
2018-11-16 10:43:51 -0700

    working on header html, basic reset for html page

---
+++ src/components/Header.jsx
@@ -0,0 +1,23 @@
+import React, { Component } from 'react';
+
+class Header extends Component {
+
+  render() {
+    return (
+      <div >
+        <nav>
+            <div>
+                <h1 className="title">New Reads</h1>
+            </div>
+            <div>
+                <a className="navigation" href="#">Add new book</a>
+                <a className="navigation" href="#">Recommendations</a>
+                <a className="navigation" href="#">Favorites</a>
+            </div>
+        </nav>
+      </div>
+    );
+  }
+}
+
+export default Header;
