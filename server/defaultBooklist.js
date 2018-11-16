5042d222fd5d6bb2e77e12d460eb01883922437f
mobydickens <steadmegan@gmail.com>
2018-11-16 11:18:34 -0700

    Added top 10 classics to hard coded default booklist in server

--- server/defaultBooklist.js
+++ server/defaultBooklist.js
@@ -6,7 +6,7 @@ module.exports = [
         title: "The Three Musketeers",
         author: "Alexandre Dumas",
         genre: "Adventure",
-        tags: ["Classic", "Adventure", "Romance"],
+        tags: ["Classics", "Adventure", "Romance"],
         recommended: null,
         id: 0
     },
@@ -14,19 +14,76 @@ module.exports = [
         title: "Moby Dick",
         author: "Herman Melville",
         genre: "Fiction",
-        tags: ["Classic", "Adventure"],
+        tags: ["Classics", "Adventure"],
         recommended: null,
         id: 1
     },
-    {},
-    {},
-    {},
-    {},
-    {},
-    {},
-    {},
-    {},
+    {
+        title: "1984",
+        author: "George Orwell",
+        genre: "Fiction",
+        tags: ["Fiction", "Politics", "Dystopia", "Classics"],
+        recommended: null,
+        id: 2
+    },
+    {
+        title: "Crime and Punishment",
+        author: "Fyodor Dostoyevsky",
+        genre: "Fiction",
+        tags: ["Fiction", "Philosophy", "Classics"],
+        recommended: null,
+        id: 3
+    },
+    {
+        title: "Frankenstein",
+        author: "Mary Shelley",
+        genre: "Fiction",
+        tags: ["Fiction", "Horror", "Classics"],
+        recommended: null,
+        id: 4
+    },
+    {
+        title: "A Tale of Two Cities",
+        author: "Charles Dickens",
+        genre: "Fiction",
+        tags: ["Historical Fiction", "Fiction", "Classics"],
+        recommended: null,
+        id: 5
+    },
+    {
+        title: "The Adventures of Huckleberry Finn",
+        author: "Mark Twain",
+        genre: "Fiction",
+        tags: ["Fiction", "Adventure", "Classics"],
+        recommended: null,
+        id: 6
+    },
+    {
+        title: "Jane Eyre",
+        author: "Charlotte Bronte",
+        genre: "Romance",
+        tags: ["Romance", "Fiction", "Classics"],
+        recommended: null,
+        id: 7
+    },
+    {
+        title: "The Picture of Dorian Gray",
+        author: "Oscar Wilde",
+        genre: "Fiction",
+        tags: ["Fiction", "Classics"],
+        recommended: null,
+        id: 8
+    },
+    {
+        title: "The Great Gatsby",
+        author: "F. Scott Fitzgerald",
+        genre: "Fiction",
+        tags: ["Fiction", "Classics"],
+        recommended: null,
+        id: 9
+    },
     //10 science fiction
+    
 ]
 
 
