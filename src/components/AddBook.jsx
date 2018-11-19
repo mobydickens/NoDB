3eaf972508a3de4558991aef39d46b64077a7300
mobydickens <steadmegan@gmail.com>
2018-11-19 11:38:36 -0700

    Another API request to get quote of the day in AddBook Component

--- src/components/AddBook.jsx
+++ src/components/AddBook.jsx
@@ -1,7 +1,6 @@
 import React, { Component } from 'react';
-// import axios from 'axios';
 import DropdownBtn from './DropdownBtn.jsx';
-// import axios from 'axios';
+import axios from 'axios';
 
 //Completes ONE stateful component
 class AddBook extends Component {
@@ -12,10 +11,21 @@ class AddBook extends Component {
             title: "",
             author: "",
             genre: "",
-            recommended: ""
+            recommended: "",
+            quoteOfTheDay: "",
+            authorOfQuote: ""
         }
   }
   
+componentDidMount() {
+    axios.get("http://quotes.rest/qod.json").then(res => {
+        this.setState({
+            quoteOfTheDay: res.data.contents.quotes[0].quote,
+            authorOfQuote: res.data.contents.quotes[0].author
+        })   
+    })
+}
+
   getBook = () => {
     this.props.addBookFn(this.state)
     this.setState({
@@ -58,7 +68,9 @@ class AddBook extends Component {
                 value={ this.state.recommended }  
                 onChange={ (e) => this.setState({ recommended: e.target.value })} /><br/>
             <button className="btn btn-primary" onClick={ () => this.getBook()} > Add </button>
-            <div className="dictionary-title">Word of the Day</div>
+            <div className="dictionary-title">Quote of the Day</div>
+            <div>{ this.state.quoteOfTheDay }</div>
+            <div>~ { this.state.authorOfQuote }</div>
         </main>
       </div>
     );
