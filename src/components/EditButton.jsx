14d7d7afe3868f8818d1ef24d76d65700cf7c5ff
mobydickens <steadmegan@gmail.com>
2018-11-18 22:26:15 -0700

    Making modal edit work and styling slightly

--- src/components/EditButton.jsx
+++ src/components/EditButton.jsx
@@ -36,27 +36,69 @@ class EditButton extends Component {
             </div>
             
                 <div 
-                    class="modal fade"
+                    className="modal fade"
                     id={ "book" + this.state.id }
                     tabindex="-1"
                     role="dialog"
                     aria-labelledby="exampleModalCenterTitle"
                     aria-hidden="true">
-                    <div class="modal-dialog modal-dialog-centered" role="document">
-                        <div class="modal-content">
-                            <div class="modal-header">
-                                <h5 class="modal-title" id="exampleModalCenterTitle">Modal title</h5>
-                                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                    <div className="modal-dialog modal-dialog-centered" role="document">
+                        <div className="modal-content">
+                            <div className="modal-header">
+                                <h5 className="modal-title" id="exampleModalCenterTitle">Modal title</h5>
+                                <button type="button" className="close" data-dismiss="modal" aria-label="Close">
                                     <span aria-hidden="true">&times;</span>
                                 </button>
                             </div>
-                        <div class="modal-body">
-                            ...
+                <div className="modal-body">
+                    <div className="card card-body" id={ "book" + this.state.id }>
+
+                        <div className="form-group">
+                            <label htmlFor="edit">Title</label>
+                            <input 
+                                className="form-control"
+                                type="text"
+                                value={ this.state.title || ""}
+                                onChange={ (e) => this.setState({ title: e.target.value }) } />
                         </div>
-                        <div class="modal-footer">
-                            <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
-                            <button type="button" class="btn btn-primary">Save changes</button>
+                        
+                        <div className="form-group">
+                            <label htmlFor="edit1">Author</label>
+                            <input 
+                                className="form-control"
+                                type="text" 
+                                value={ this.state.author || ""}
+                                onChange={ (e) => this.setState({ author: e.target.value }) } />
                         </div>
+                        
+                        <div className="form-group">
+                            <label htmlFor="edit2">Genre</label>
+                            <input 
+                                className="form-control"
+                                type="text" 
+                                value={ this.state.genre || ""}
+                                onChange={ (e) => this.setState({ genre: e.target.value })} />
+                        </div>
+                        
+                        <div className="form-group">
+                            <label htmlFor="edit3">Recommended by...</label>
+                            <input 
+                                className="form-control"
+                                type="text" 
+                                value={ this.state.recommended || ""}
+                                onChange={ (e) => this.setState({ recommended: e.target.value || "" })} />
+                        </div>
+                        <button
+                            className="btn btn-secondary" 
+                            onClick={ () => this.editBook() }>
+                            Save
+                        </button>
+                    </div>
+                </div>
+                        </div>
+                        <div className="modal-footer">
+                            <button type="button" className="btn btn-secondary" data-dismiss="modal">Close</button>
+                            <button type="button" className="btn btn-primary">Save changes</button>
                         </div>
                     </div>
                 </div>
