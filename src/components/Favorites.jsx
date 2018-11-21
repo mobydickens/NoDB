6ab5cb9062a64656898b23518ba07f157d99b806
mobydickens <steadmegan@gmail.com>
2018-11-21 09:36:01 -0700

    Finally found a design I like for the Favorites page

--- src/components/Favorites.jsx
+++ src/components/Favorites.jsx
@@ -10,13 +10,19 @@ class Favorites extends Component {
         const book = this.props.userBooklist.map(book => {
             //need to check if list is EMPTY, and return a prompt if so (maybe a button to take you to the AddBook Page)
             return(
-                <div key={book.id} className="parentforfav">
+                <div key={book.id} className="individual-parent">
+                    <div className="book-parent">
+                        <div 
+                            className="book"
+                            style={{ backgroundImage: "url(/covers/" + book.id + ")"}}>
+                            
+                        </div>
+                    </div>
                     <div className="text-box">
                         <div 
                             className="title">
                             {book.title}
-                        </div><br/>
-                        by 
+                        </div>
                         <div
                             className="author">
                             {book.author}
@@ -24,23 +30,17 @@ class Favorites extends Component {
                             {!book.genre ? "" : <div className="genre">{book.genre}</div>}
                             {!book.recommended ? "" : <div className="rec">Recommended by:<br/> {book.recommended}
                         </div>}
-                    
-                    </div>
-                    <div className="favorite-btn">
-                        <DeleteBtn 
-                            deleteBookFn={ this.props.deleteBookFn}
-                            id={ book.id }/>
-                        <EditButton 
-                            title={ book.title }
-                            author={ book.author }
-                            recommended={ book.recommended }
-                            id={ book.id }
-                            editBookFn={ this.props.editBookFn }/>
-                    </div>
-                    <div 
-                        className="book"
-                        style={{ backgroundImage: "url(/covers/" + book.id + ")"}}>
-                        
+                        <div className="favorite-btn">
+                            <DeleteBtn 
+                                deleteBookFn={ this.props.deleteBookFn}
+                                id={ book.id }/>
+                            <EditButton 
+                                title={ book.title }
+                                author={ book.author }
+                                recommended={ book.recommended }
+                                id={ book.id }
+                                editBookFn={ this.props.editBookFn }/>
+                        </div>
                     </div>
                 </div>)
         })
@@ -48,7 +48,7 @@ class Favorites extends Component {
         return ( 
             <div className="container">
                 <div className="subtitle">To-read books in favorites</div>
-                <div className="book-parent">
+                <div className="favorites-parent">
                     {book.reverse()}
                 </div>
             </div>
